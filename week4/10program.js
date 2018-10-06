ed98d819a084b302700fecd0138e90a56d3d2dfd
Louise <louise.ban@gmail.com>
2018-10-06 19:47:04 +0200

    .

--- week4/10program.js
+++ week4/10program.js
@@ -1,5 +1,7 @@
 var program = [23, "this is a string", 13, "yellow",];
 console.log(program);
-console.log(Infinity + Infinity);
+
 var compare = 6/0 === 10/0;
 console.log(compare);
+var compareneg = -6/0 === 10/0;
+console.log(compareneg);
